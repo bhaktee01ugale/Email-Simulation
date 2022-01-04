6bcfed8793fdd74322dfd3564466bef14866093d
Bhaktee Ugale <bhakteeugale01dec@gmail.com>
2022-01-04 18:11:58 +0530

    Updated classes

--- Email_Simulation.py
+++ Email_Simulation.py
@@ -12,23 +12,52 @@ from tkinter import messagebox
 # yashvi
 class menu_window(ttk.Frame):
     def __init__(self, container):
-        super().__init__(container)
+        super().__init__(container)  # to invoke parent constructor
+
+        #container.geometry('560x378')
 
         def create(self):
             compose_window(container).grid(row=0,column=0)
-            self.destroy()
+            compose_window.tkraise(container)
 
+        style = Style()
 
-        self.l = ttk.Label(self, width=30).grid(row=0, column=0)
-        self.create = ttk.Button(self, text='Compose', command=lambda: create(self), cursor='hand2').grid(row=1, column=1)
+        style.configure('W.TButton', font=
+        ('Helvetica', 15, 'bold'),
+                        foreground='black')
+
+        self.l = ttk.Label(self,width=10)
+        self.l.grid(row=0, column=0)
+        self.l.configure(foreground='black', font='Helvetica 20')
+        self.create = ttk.Button(self, text='Compose', command=lambda: create(self), cursor='hand2', style='W.TButton').grid(row=1, column=1)
         self.rowconfigure(0, weight=3)
-        self.columnconfigure(0, weight=3)
+        self.columnconfigure(0, weight=2)
 
 # yashvi, snehal # same as display draft
 class compose_window(ttk.Frame):
+    def temp_text_to(self, e):
+        self.to.delete(0, "end")
+        self.to.configure(foreground='black')
+
+    def temp_text_sub(self, e):
+        self.sub.delete(0, "end")
+        self.sub.configure(foreground='black')
+
+    def on_focusout_to(self, e):
+        if self.to.get() == '':
+            #self.to.delete(0, "end")
+            self.to.insert(0, 'Recipient')
+            self.to.configure(foreground='grey')
+
+    def on_focusout_sub(self, e):
+        if self.sub.get() == '':
+            #self.input_password.delete(0, "end")
+            self.sub.insert(0, 'Subject')
+            self.sub.configure(foreground='grey')
+
     def __init__(self, container):
-        super().__init__(container)
-        container.geometry('500x520')
+        super().__init__(container)  # to invoke parent constructor
+        container.geometry('555x555')
 
         def send(self):
             try:
@@ -40,79 +69,87 @@ class compose_window(ttk.Frame):
                 print("Subject line:",subject)
                 global body
                 body = body + self.body.get("1.0", 'end-1c')
-                print(body)
+                print("Body of email:",body)
                 global smtp
+                # To create message
                 msg = MIMEMultipart()
                 msg['From'] = sender_email
                 msg['To'] = receiver_email
                 msg['Subject'] = subject
                 msg.attach(MIMEText(body))
 
-                smtp.send_message(msg)
 
-                smtp.quit()
+                smtp.send_message(msg)  # To send email
+
+                smtp.quit() # to disconnect from the server
 
                 print("Email sent Successfully!")
                 messagebox.showinfo("Status", "Email sent Successfully!")
+                container.destroy()
             except Exception as ex:
                 messagebox.showerror("Error", "Something went wrong!")
                 print('Something went wrong!', ex)
 
-        self.t_l = ttk.Label(self, text="To").grid(row=0, column=0)
-        self.to = ttk.Entry(self, width=10)
-        self.to.grid(row=0, column=1)
+        # Recipient field
+        self.to = ttk.Entry(self, width=50)
+        self.to.grid(row=0, column=0)
+        self.to.insert(0, "Recipient")
+        self.to.bind("<FocusIn>", self.temp_text_to)
+        self.to.bind("<FocusOut>", self.on_focusout_to)
+        self.to.configure(foreground='grey', font='Helvetica 15')
         self.to.get()
-        self.s_l = ttk.Label(self, text="Subject").grid(row=1, column=0)
-        self.sub = ttk.Entry(self)
-        self.sub.grid(row=1, column=1)
+
+        # Subject line
+
+        #self.s_l = ttk.Label(self, text="Subject").grid(row=1, column=0)
+        self.sub = ttk.Entry(self, width=50)
+        self.sub.grid(row=1, column=0)
+        self.sub.insert(0, "Subject")
+        self.sub.bind("<FocusIn>", self.temp_text_sub)
+        self.sub.bind("<FocusOut>", self.on_focusout_sub)
+        self.sub.configure(foreground='grey', font='Helvetica 15')
         self.sub.get()
 
+        # Body for email
         self.body = tk.Text(self, width=50)
-        self.body.grid(row=2, columnspan=2)
+        self.body.grid(row=2, column=0)
         self.body.get("1.0", 'end-1c')
 
-        self.send_button = ttk.Button(self, text="Send", cursor='hand2',command=lambda: send(self)).grid(row=3, column=1)
+        self.l = ttk.Label(self, width=50).grid(row=3, column=0)
 
+        style = Style()
 
+        style.configure('W.TButton', font=
+        ('Helvetica', 15, 'bold'),
+                        foreground='black')
 
-# jessica
-class display_sent_window(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
-
-
-# bhaktee # same as sent window
-class drafts_window(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
-
-
-
-#bhaktee
-class sent_winodw(ttk.Frame):
-    def __init__(self, container):
-        super().__init__(container)
+        # send button
+        self.send_button = ttk.Button(self, text="Send", cursor='hand2',command=lambda: send(self), style='W.TButton').grid(row=4, column=0)
 
 
 
 # jessica
 class login_Window(ttk.Frame):
+
+    # to delete default text in username field
     def temp_text_user(self, e):
         self.input_username.delete(0, "end")
         self.input_username.configure(foreground='black')
 
+    # to delete default text in password field
     def temp_text_pass(self, e):
         self.input_password.delete(0, "end")
         self.input_password.configure(foreground='black')
         self.input_password.configure(show="*")
 
+    # to enter default text in password field
     def on_focusout_pass(self, e):
         if self.input_password.get() == '':
             #self.input_password.delete(0, "end")
             self.input_password.insert(0, 'Password')
             self.input_password.configure(foreground='grey')
 
+    # to enter default text in username field
     def on_focusout_user(self, e):
         if self.input_username.get() == '':
             #self.input_password.delete(0, "end")
@@ -120,10 +157,7 @@ class login_Window(ttk.Frame):
             self.input_username.configure(foreground='grey')
 
     def __init__(self, container):
-        super().__init__(container)
-
-        self.sender = ''
-        self.password = ''
+        super().__init__(container) # to invoke parent constructor
 
         def set(self):
             global sender_email
@@ -132,47 +166,60 @@ class login_Window(ttk.Frame):
             password = password + self.input_password.get()
             try:
                 global smtp
-                smtp.login(sender_email, password)
+
+                smtp.login(sender_email, password)  # to login with given credentials
+
                 print("Logged in Successfully!")
                 messagebox.showinfo("Status", "Logged In Successfully!")
                 self.input_password.destroy()
                 self.input_username.destroy()
-                menu_window(container).grid(row=0, column=0)
+                menu_window(container).grid(row=0, column=0)  # to create menu window
                 self.destroy()
             except Exception as ex:
-                messagebox.showerror("Error", "Wrong Credentials!")
+                messagebox.showerror("Error", "Wrong Credentials!\nRestart program!")
                 print('Something went wrong!', ex)
 
         container.geometry('565x378')
+
+        self.ph = ttk.Label(self, text="Welcome to Mail")
+        self.ph.grid(row=1, column=1)
+        self.ph.configure(foreground='black', font='Helvetica 20')
+
+        # username field
         self.input_username = ttk.Entry(self, width=35)
-        self.input_username.grid(row=1, column=1)
+        self.input_username.grid(row=3, column=1)
         self.input_username.insert(0, "Username")
         self.input_username.bind("<FocusIn>", self.temp_text_user)
         self.input_username.bind("<FocusOut>", self.on_focusout_user)
         self.input_username.configure(foreground='grey', font='Helvetica 15')
         self.input_username.get()
 
+        # password field
         self.input_password = ttk.Entry(self, width=35)
-        self.input_password.grid(row=2, column=1)
+        self.input_password.grid(row=4, column=1)
         self.input_password.insert(0, "Password")
         self.input_password.bind("<FocusIn>", self.temp_text_pass)
         self.input_password.bind("<FocusOut>", self.on_focusout_pass)
         self.input_password.configure(foreground='grey', font='Helvetica 15')
         self.input_password.get()
 
+        # to create style object
         style = Style()
 
         style.configure('W.TButton', font=
         ('Helvetica', 15, 'bold'),
                         foreground='black')
-        self.Login = ttk.Button(self, text='LOGIN', command=lambda : set(self), cursor='hand2', style='W.TButton').grid(row=4, column=1)
+
+        # login button
+        self.Login = ttk.Button(self, text='Login', command=lambda : set(self), cursor='hand2', style='W.TButton').grid(row=6, column=1)
         #self.Login['Font'] = self.myFont
 
 
         self.t = ttk.Label(self, width=10).grid(row=0, columnspan=3)
         self.t_c = ttk.Label(self, width=10).grid(row=1, column=0)
-        self.b_r = ttk.Label(self, width=10).grid(row=3, columnspan=3)
+        self.b_r = ttk.Label(self, width=10).grid(row=5, columnspan=3)
         self.columnconfigure(index=0, weight=2)
+        self.w = ttk.Label(self, width=10).grid(row=2, column=0)
 
         #self.grid(row=0, column=0)
 
@@ -181,28 +228,36 @@ class login_Window(ttk.Frame):
 # bhaktee
 class Start_Window(ttk.Frame):
     def __init__(self, container):
-        super().__init__(container)
+        super().__init__(container)  # to invoke parent constructor
 
         file = 'mail-download.gif'
-        info = Image.open(file)
-        frames = info.n_frames
+        info = Image.open(file)  # to open the gif
+        frames = info.n_frames # to find number of photos of gif
 
-        im = [PhotoImage(file=file, format=f'gif -index {i}') for i in range(frames)]
+        im = [PhotoImage(file=file, format=f'gif -index {i}') for i in range(frames)]  # list to store photos of gif
 
-        count = 0
+        count = 0  # to save index of images
 
+        # to run the gif
         def animation(count):
-            im2 = im[count]
-            count += 1
-            if count == frames:
-                self.gif_label.destroy()
-                login_Window(container).place(anchor=tk.NW)
-                self.destroy()
-                return
-            self.gif_label.configure(image=im2)
-            self.after(50, animation, count)
 
-        self.gif_label = ttk.Label(self)
+            im2 = im[count] # to access the image of index count
+
+            count += 1
+
+            # if displaying of all photos is complete
+            if count == frames:
+                self.gif_label.destroy()  # destroy the label which displays gif
+
+                login_Window(container).place(anchor=tk.NW)   # creating login_window
+
+                self.destroy()  # to destroy the current window
+                return
+
+            self.gif_label.configure(image=im2)  # to display image
+            self.after(50, animation, count) # to run
+
+        self.gif_label = ttk.Label(self)  # label to display gif
         self.gif_label.pack()
 
         self.after(0, animation, 0)
@@ -211,15 +266,17 @@ class Start_Window(ttk.Frame):
 
 class App(tk.Tk):
     def __init__(self):
-        super().__init__()
+        super().__init__() # to invoke parent constructor
 
-        self.geometry('500x378')
+        self.geometry('500x378') #to set the size of winodow
 
-        icon = PhotoImage(file='Mail.png')
+        icon = PhotoImage(file='Mail.png') # to open icon image
 
-        self.title("Mail")
+        self.title("Mail")  # to set title
 
-        self.iconphoto(False, icon)
+        self.iconphoto(False, icon) # to set icon of tkinter window
+
+        self.resizable(False, False)  # to disable resize button
 
         '''self.frames = {}
         for F in (Start_Window, login_Window, compose_window):
@@ -240,17 +297,18 @@ class App(tk.Tk):
 
 
 if __name__ == "__main__" :
+
     try:
-        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)  # to set connection with server
         print("Session Started!")
 
-        smtp.starttls()
+        smtp.starttls()  # to securely connect with server
         app = App()
-        sender_email = ''
-        password = ''
-        receiver_email = ''
-        body = ""
-        subject=''
+        sender_email = ''   # email address of the sender
+        password = ''   # password of the sender's account
+        receiver_email = '' # email address of the receiver
+        body = ""   # body of email
+        subject=''  # subject of email
         app.mainloop()
     except Exception as ex:
         messagebox.showerror("Error", "No internet connectivity!")
